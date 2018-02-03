43477409324776c5a7e25cdb3c323cc6d102a71c
Tim McArdle <TimMcArdle@Tims-MacBook-Pro.local>
2018-02-03 17:54:59 -0600

    resolved naming

--- homework-1/sigmoid.py
+++ homework-1/sigmoid.py
@@ -1,6 +1,10 @@
 from scipy.special import expit as sigmoidFunction
 
-def sigmoid_wrapper(z):
-    return simgoidFunction(z)
 
-print(sigmoid_wrapper(0))
+# sedrfgtyhu
+def sigmoid_wrapper(z):
+    # asedrftgyh
+    return sigmoidFunction(z)
+
+
+print sigmoid_wrapper(0)
